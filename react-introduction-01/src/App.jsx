f8c5ab64d449946cb8e5f46c4964d745034735d4
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-08-22 00:46:29 +0500

    create simple react app

---
+++ react-introduction-01/src/App.jsx
@@ -0,0 +1,15 @@
+// react ka under hum apna function ko compnent khata hain 
+// or component ka phala leter bara hoga 
+
+// framwork vs librayies
+
+// 1: framwork pr set of rules define hota 
+// 2: libries pr set of rules define nahi hota
+
+function App(){
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+export default App
