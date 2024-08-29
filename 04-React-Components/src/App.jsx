fa089980e8d48d3bd4d7ce30a1d81222367b01c5
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-08-29 11:03:50 +0500

    React-component-practice

--- 04-React-Components/src/App.jsx
+++ 04-React-Components/src/App.jsx
@@ -1,11 +1,13 @@
 import Card from "./Components/card"
 import Navbar from "./Components/navbar"
+import Button from "./Components/Button"
 
 function App() {
     return (
         <>
             <Navbar />
-            <div className="flex justify-center">
+            <Button />
+            <div className="flex justify-between mx-4 mt-4">
                 <Card />
                 <Card />
                 <Card />
--- 04-React-Components/src/Components/Card.jsx
+++ 04-React-Components/src/Components/Card.jsx
@@ -1,8 +1,23 @@
 function Card() {
     return (
         <>
-            <div className="w-[300px] h-[300px] mx-3 mt-3 bg-slate-500">
-                <h1>hello world</h1>
+            <div className="card bg-base-100 w-96 shadow-xl">
+                <figure>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        Shoes!
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">Fashion</div>
+                        <div className="badge badge-outline">Products</div>
+                    </div>
+                </div>
             </div>
         </>
     )
