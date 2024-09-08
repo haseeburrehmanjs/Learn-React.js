216867628097b0ce9e091f67f5528b40fd337747
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-09-08 15:52:41 +0500

    react-router-dom-practice

--- 00-React-Project/React-Quiz-App/src/App.jsx
+++ 00-React-Project/React-Quiz-App/src/App.jsx
@@ -1,34 +1,33 @@
-// import axios from 'axios'
-// import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
-// const App = () => {
-//   const [questions, setquestions] = useState([])
+const App = () => {
+    let [quizData, setquizData] = useState([])
+    const [index, setindex] = useState(0)
 
-//   // get data from database
-//   useEffect(() => {
-//     axios('https://the-trivia-api.com/v2/questions')
-//     .then(res => {
-//       // console.log(res.data);
-//       setquestions([res.data])
-//       console.log(questions);
-      
-//     }).catch(error => {
-//       console.log(error);
-//     })
-//   }, [])
+    useEffect(() => {
+        async function getDataFromDb() {
+            let question = await axios('https://the-trivia-api.com/v2/questions')
+            console.log(question.data[index].question.text);
+            setquizData(question.data)
+        }
+        getDataFromDb()
+    }, [])
 
-
-//   return (
-//     <>
-//       <h1>Quiz</h1>
-//       <div>
-//         {questions ? <h1>{questions.data}</h1> : 'loading...'}
-//       </div>
-//     </>
-//   )
-// }
-
-// export default App
+    function indexChange() {
+        index < quizData.length - 1 ? setindex(index + 1) : alert('question khatum ho gain hain')
+    }
 
 
+    return (
+        <>
+            <h1>Hello World {index}</h1>
+            <div>
+                {quizData.length > 0 ? <h1>Q{index + 1}: {quizData[index].question.text}</h1> : <h1>Loading...</h1>}
+            </div>
+            <button onClick={indexChange}>Next</button>
+        </>
+    )
+}
 
+export default App
