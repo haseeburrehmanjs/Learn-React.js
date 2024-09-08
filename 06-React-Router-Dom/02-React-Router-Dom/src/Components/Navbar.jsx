eb6728dfea8f28ba2db7a43e37d8c87bf794db79
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-09-08 20:32:25 +0500

    react-dom-practice

--- 06-React-Router-Dom/02-React-Router-Dom/src/Components/Navbar.jsx
+++ 06-React-Router-Dom/02-React-Router-Dom/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import Contact from '../Pages/Contact'
 const Navbar = () => {
     return (
         <>
-            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <nav className="bg-blue-700 border-gray-200">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a
                         href="https://flowbite.com/"
@@ -45,11 +45,11 @@ const Navbar = () => {
                         </svg>
                     </button>
                     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-whitdark:border-gray-700">
                             <li>
                                 <Link
-                                    to=""
-                                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                                    to="/"
+                                    className="block py-2 px-3 text-white bg-white-800 rounded md:bg-transparent md:text-white-700 md:p-0 dark:text-white hover:text-blue-500 md:dark:text-white-500"
                                     aria-current="page"
                                 >
                                     Home
--- 06-React-Router-Dom/02-React-Router-Dom/src/main.jsx
+++ 06-React-Router-Dom/02-React-Router-Dom/src/main.jsx
@@ -13,6 +13,10 @@ const router = createBrowserRouter([
     element : <Layout/>,
     children : [
       {
+        path : '',
+        element : <Home/>
+      },
+      {
         path : 'about',
         element : <About/>
       },
